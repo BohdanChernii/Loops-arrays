76babb82641584844cfb416d332e0c6fd8c26cab
promius <64136178+BohdanChernii@users.noreply.github.com>
2020-06-01 12:29:49 +0300

    Create exersizes.js

---
+++ exersizes.js
@@ -0,0 +1,5 @@
+first:https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript?fbclid=IwAR3YI859W93y0_kzYKnFWQPKMYTPngUBndr0OjOoqGD05E9f4Wk9kBN7Dko
+
+function stringToArray(string){
+  return string.split(' ');
+}
